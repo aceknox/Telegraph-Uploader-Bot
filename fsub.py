9e3667df0a1a5b64ca6419f7a32c7491b1bc88e7
knox <111348305+aceknox@users.noreply.github.com>
2023-02-23 09:55:33 +0530

    Update fsub.py

--- fsub.py
+++ fsub.py
@@ -1,66 +1,47 @@
 import asyncio
-from typing import (
-    Union
-)
-from configs import Config
+from config import Config
 from pyrogram import Client
 from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-
-async def get_invite_link(bot: Client, chat_id: Union[str, int]):
+async def handle_force_subscribe(bot, message):
     try:
-        invite_link = await bot.create_chat_invite_link(chat_id=chat_id)
-        return invite_link
+        invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
     except FloodWait as e:
-        print(f"Sleep of {e.value}s caused by FloodWait ...")
-        await asyncio.sleep(e.value)
-        return await get_invite_link(bot, chat_id)
-
-
-async def handle_force_sub(bot: Client, cmd: Message):
-    if Config.UPDATES_CHANNEL and Config.UPDATES_CHANNEL.startswith("-100"):
-        channel_chat_id = int(Config.UPDATES_CHANNEL)
-    elif Config.UPDATES_CHANNEL and (not Config.UPDATES_CHANNEL.startswith("-100")):
-        channel_chat_id = Config.UPDATES_CHANNEL
-    else:
-        return 200
+        await asyncio.sleep(e.x)
+        return 400
     try:
-        user = await bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
+        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
         if user.status == "kicked":
             await bot.send_message(
-                chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/knoxsupport).",
-                disable_web_page_preview=True
+                chat_id=message.from_user.id,
+                text="Sorry , You are Banned. Contact My [Support Group](https://t.me/knoxbots).",
+                parse_mode="markdown",
+                disable_web_page_preview=True,
+                reply_to_message_id=message.message_id,
             )
             return 400
     except UserNotParticipant:
-        try:
-            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
-        except Exception as err:
-            print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
-            return 200
         await bot.send_message(
-            chat_id=cmd.from_user.id,
-            text="**Join My Updates Channel to use this Bot!**\n\n"
-                 "Only Channel Subscribers can use this Bot!",
+            chat_id=message.from_user.id,
+            text="**Please Join My Update Channel To Use Me**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Join Updates Channel", url=invite_link.invite_link)
-                    ],
-                    [
-                        InlineKeyboardButton("Refresh", callback_data="refreshForceSub")
+                        InlineKeyboardButton("Join My Update Group ", url=invite_link.invite_link)
                     ]
                 ]
-            )
+            ),
+            parse_mode="markdown",
+            reply_to_message_id=message.message_id,
         )
         return 400
     except Exception:
         await bot.send_message(
-            chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/knoxsupport).",
-            disable_web_page_preview=True
+            chat_id=message.from_user.id,
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/knoxbots).",
+            parse_mode="markdown",
+            disable_web_page_preview=True,
+            reply_to_message_id=message.message_id,
         )
-        return 200
-    return 200
+        return 400
