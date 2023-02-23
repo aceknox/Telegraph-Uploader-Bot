c97af908202c8bd042e67b0f950e564aad36948c
knox <111348305+aceknox@users.noreply.github.com>
2023-02-23 10:06:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
      #updates channel
      UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","-1001509464345")
-     LOG_CHANNEL = os.environ.get("LOG_CHANNEL","-1001834539520"
+     LOG_CHANNEL = os.environ.get("LOG_CHANNEL","-1001834539520")
