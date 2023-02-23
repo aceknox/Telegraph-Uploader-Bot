cc759a61ea9424acf41ac94910211853a430a771
knox <111348305+aceknox@users.noreply.github.com>
2023-02-23 09:42:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config(object):
      #updates channel
-     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL",)
+     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","-1001509464345")
